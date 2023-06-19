f232ab5e325f62825a0becf7971150a8f6f25931
OlleAckebjer <olle.ackebjer@gmail.com>
2023-06-19 12:13:19 +0200

    test

--- README.md
+++
@@ -1,1 +0,0 @@
-# Rullande-Tid
---
+++ test.py
@@ -0,0 +1,37 @@
+import usb.core
+import usb.util
+
+# Find the USB device
+
+vendor_id=""
+product_id=""
+
+def read_data():
+    device = usb.core.find(idVendor=vendor_id, idProduct=product_id)
+
+    # Check if the device is found
+    if device is None:
+        raise ValueError("USB device not found.")
+
+    # Set the active configuration of the USB device
+    device.set_configuration()
+
+    # Find the input endpoint (assuming it's the first endpoint)
+    endpoint = device[0][(0, 0)][0]
+
+    # Read data from the USB device
+    data = device.read(endpoint.bEndpointAddress, endpoint.wMaxPacketSize)
+
+    # Process the data as needed
+    print(data)
+
+def find_usb_data():
+    for device in usb.core.find(find_all=True):
+        # Get the VID and PID
+        vid = hex(device.idVendor)
+        pid = hex(device.idProduct)
+
+        # Print the VID and PID
+        print(f"Device: VID={vid}, PID={pid}")
+
+find_usb_data()
